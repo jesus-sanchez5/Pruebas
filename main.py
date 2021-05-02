64577151c04e123e7995009055b5520e07817083
Jesus Sanchez <chusete777@gmail.com>
2021-05-02 19:07:04 +0200

    Cambios

--- main.py
+++ main.py
@@ -4,13 +4,25 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(name):
+def suma(num1, num2):
     # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    print(f'{num1+num2}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+def resta(num1, num2):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'{num1-num2}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+def division(num1, num2):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f' Cociente:{num1/num2}')  # Press Ctrl+F8 to toggle the breakpoint.
+    print(f' Resto:{num1%num2}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    print('Elige una opcion:')
+    suma(1,3)
+    resta(3,2)
+    division(5,2)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
