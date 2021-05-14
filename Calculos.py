fb8933e79c6d06cb19adc9cf6de5df07f581ed30
Jesus Sanchez <chusete777@gmail.com>
2021-05-14 14:38:18 +0200

    Prueba 1 hoy

---
+++ Calculos.py
@@ -0,0 +1,19 @@
+def suma():
+    print(f'Sumar')
+    num1 = float(input('Introduce el primer valor: '))
+    num2 = float(input('Introduce el segundo valor: '))
+    print(f'La suma es: {num1+num2}')
+
+def resta():
+    print(f'Restar')
+    num1 = float(input('Introduce el primer valor: '))
+    num2 = float(input('Introduce el segundo valor: '))
+    print(f'La resta es: {num1 - num2}')
+
+
+
+def division():
+    print('Dividir')
+    num1 = float(input('Introduce el primer valor: '))
+    num2 = float(input('Introduce el segundo valor: '))
+    print(f'La division es: {num1/num2}')
--- main.py
+++ main.py
@@ -1,28 +1,15 @@
-# This is a sample Python script.
-
-# Press Mayús+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import Calculos
 
 
-def suma(num1, num2):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'{num1+num2}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-def resta(num1, num2):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'{num1-num2}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-def division(num1, num2):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f' Cociente:{num1/num2}')  # Press Ctrl+F8 to toggle the breakpoint.
-    print(f' Resto:{num1%num2}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print('Elige una opcion:')
-    suma(1,3)
-    resta(3,2)
-    division(5,2)
+    print('1 - Sumar\n2 - Restar')
+    opcion= int(input('Elige una opcion: '))
+    if opcion==1:
+        Calculos.suma()
+    if opcion==2:
+        Calculos.resta()
+    if opcion==3:
+        Calculos.division()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+
