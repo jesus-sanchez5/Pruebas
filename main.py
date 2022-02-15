6089bb38b25a01147c6750d377490e5005dc9ec2
Jesus Sanchez <chusete777@gmail.com>
2022-02-15 18:57:51 +0100

    15

--- main.py
+++ main.py
@@ -2,13 +2,15 @@ import Calculos
 
 
 if __name__ == '__main__':
-    print('1 - Sumar\n2 - Restar')
+    print('1 - Sumar\n2 - Restarr\n3 - Division')
     opcion= int(input('Elige una opcion: '))
     if opcion==1:
         Calculos.suma()
     if opcion==2:
         Calculos.resta()
     if opcion==3:
+        Calculos.division()
+    if opcion==4:
         Calculos.division()
 
 
