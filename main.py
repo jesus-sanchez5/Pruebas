94b58aa9372260b5308cdb589423c7f3ae91fe2f
Jesus Sanchez <chusete777@gmail.com>
2022-02-15 19:06:34 +0100

    Implementar salir

--- main.py
+++ main.py
@@ -2,16 +2,20 @@ import Calculos
 
 
 if __name__ == '__main__':
-    print('1 - Sumar\n2 - Restar\n3 - Division\n4 - Multiplicacion')
-    opcion= int(input('Elige una opcion: '))
-    if opcion==1:
-        Calculos.suma()
-    if opcion==2:
-        Calculos.resta()
-    if opcion==3:
-        Calculos.division()
-    if opcion==4:
-        Calculos.multiplicacion()
+    i = 0
+    while i != 1:
+        print('1 - Sumar\n2 - Restar\n3 - Division\n4 - Multiplicacion\n\n0 - Salir')
+        opcion= int(input('Elige una opcion: '))
+        if opcion==1:
+            Calculos.suma()
+        if opcion==2:
+            Calculos.resta()
+        if opcion==3:
+            Calculos.division()
+        if opcion==4:
+            Calculos.multiplicacion()
+        if opcion==0:
+            i=1
 
 
 
